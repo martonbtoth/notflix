34cb9a5e87d937eafa306ffd0755f24e3fcf6599
Marton Toth <toth.marton33@gmail.com>
2016-11-22 17:20:03 +0100

    More react stuff

--- gateway/src/main/resources/static/js/src/App.jsx
+++ gateway/src/main/resources/static/js/src/App.jsx
@@ -1,12 +1,27 @@
 import * as React from "react";
+import Header from "./Header.jsx";
+import * as axios from "axios";
 
 function Placeholder(props) {
     return <img src={'http://placehold.it/' + props.x + 'x' + props.y}/>;
 }
 
+
 export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount() {
+        let _this = this;
+        axios.get('/api/users/me').then((response) => _this.setState({username: response.data.username}));
+    }
+
     render() {
         return <div>
+            <Header username={this.state.username}/>
             <h1>Hello, world!</h1>
             <Placeholder x="200" y="300"/>
         </div>;
