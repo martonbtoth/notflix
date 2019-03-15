3e3ca8129b0a19fe849f311be44e4f02d484d740
Márton Tóth <toth.marton33@gmail.com>
2019-03-15 19:54:32 +0100

    Use gateway port in development

--- frontend/src/components/Preview/Preview.js
+++ frontend/src/components/Preview/Preview.js
@@ -6,7 +6,9 @@ export class Preview extends React.Component {
 
     render() {
         return (
-            <div className="preview">
+            <div className="preview" style={{
+                "backgroundImage": `url(/api/content/${this.props.title.id}/thumbnail)`
+            }}>
                 <img className="logo" src="/logo.png"/>
                 <div className="info">
                     <div className="title">
