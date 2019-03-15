2e1aa5b243a1a5b543f3bec96b7b2601b7d65f41
Márton Tóth <toth.marton33@gmail.com>
2019-03-15 19:54:05 +0100

    Add gateway

---
+++ gateway/src/main/kotlin/hu/martontoth/gateway/Beans.kt
@@ -0,0 +1,22 @@
+package hu.martontoth.gateway
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class Beans {
+
+    @Bean
+    fun routeLocator(builder: RouteLocatorBuilder): RouteLocator {
+        return builder.routes()
+                .route("titles")
+                { it.path("/api/titles").uri("http://localhost:8080/") }
+                .route("content")
+                {it.path("/api/content/**/*").uri("http://localhost:8081/")}
+                .build()
+    }
+
+}
